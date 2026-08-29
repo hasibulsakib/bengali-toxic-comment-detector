14618bd0731abb6a2ae98634379cab19651899b9
Hasibul Sakib <hasibulsakib268@gmail.com>
2026-08-29 06:56:30 +0600

    Update app launch to use environment variables

--- app.py
+++ app.py
@@ -146,4 +146,9 @@ app = gr.Interface(
 # Launch App
 # ==============================
 
-app.launch()
+import os
+
+app.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
